050360f2f75dd7a5ba9ee10a3b66454cdb8b5723
Rafael Goncalves <rafael.krentz@hotmail.com>
2017-11-13 15:20:42 -0200

    bluetooth connecting working

--- Connect/ViewController.swift
+++ Connect/ViewController.swift
@@ -13,49 +13,50 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
     
     var centralManager:CBCentralManager!
     var blueToothReady = false
-    var connectingPeripheral: CBPeripheral!
-    var connectingCharac: CBCharacteristic!
+    var connectingPeripheral: CBPeripheral?
+    var connectingCharac: CBCharacteristic?
 
     var peripherals = Array<CBPeripheral>()
+    var response = Array<String>()
     
     
-    @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var textView: UITextView!
     
     @IBAction func refreshBLE(sender: UIButton) {
-        //   centralManager.scanForPeripherals(withServices: nil, options: nil)
         
-        var bytes = NSData(bytes: [0x5A, 0xB1, 0xFA, 0xBB, 0xC0, 0x3D, 0xFE, 0x34,0x45,0xCD,0x00,0x54,0x25,0x62,0x36,0x22] as [UInt8], length: 16)
-        var bytesData = [UInt8]("0x01".utf8)
         
-        let writeData = NSData (bytes: &bytes, length: bytesData.count)
-        let value: [UInt8] = [0x5A, 0xB1, 0xFA, 0xBB, 0xC0, 0x3D, 0xFE, 0x34,0x45,0xCD,0x00,0x54,0x25,0x62,0x36,0x22]
-        let data = NSData(bytes: value, length: 16)
-        //  let data = "01:00".data(using: String.Encoding.utf8)! as NSData
-        // let data = NSData(bytes: &bytes, length: 1)
-        self.connectingPeripheral.writeValue(data as Data, for: self.connectingCharac, type: CBCharacteristicWriteType.withResponse)
+        let value: [UInt8] = [0x5A, 0xFF, 0x00, 0x00, 0x05,0x03, 0x00, 0x00, 0x00,0x64,0x53,0x2C]
+        let data = NSData(bytes: value, length: 12)
+        print("\nValor enviado = \(data)")
         
+//                let characteristicUUID = CBUUID(string: self.connectingCharac.uuid.uuidString)
+//                let properties: CBCharacteristicProperties = [.notify, .read, .write]
+//                let permissions: CBAttributePermissions = [.readable, .writeable]
+//                let characteristic = CBCharacteristic(
+//
+//                    type: characteristicUUID,
+//                    properties: properties,
+//                    value: nil,
+//                    permissions: permissions)
+//                //service.characteristics = characteristic
+//
+//
+//
+        
+        self.connectingPeripheral!.writeValue(data as Data, for: self.connectingCharac!, type: CBCharacteristicWriteType.withoutResponse)
     }
     
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    @IBAction func showResponse(_ sender: Any) {
+        print(self.response)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        startUpCentralManager()
-    }
-    
-    func startUpCentralManager() {
         centralManager = CBCentralManager(delegate: self, queue: nil)
     }
     
-    func discoverDevices() {
-        centralManager.scanForPeripherals(withServices: nil, options: nil)
-    }
-    
+   
     func connectBluetooth(peripheral: CBPeripheral){
         centralManager.stopScan()
         centralManager.connect(peripheral, options: nil)
@@ -70,10 +71,7 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
                 print(peripheral)
             }
         }
-        self.connectingPeripheral = peripheral
         self.tableView.reloadData()
-      //  centralManager.stopScan()
-       // self.centralManager.connect(peripheral, options: nil)
         
     }
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
@@ -104,7 +102,7 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
         output(description: "State", data: msg as AnyObject)
         
         if blueToothReady {
-            discoverDevices()
+            centralManager.scanForPeripherals(withServices: nil, options: nil)
         }
     }
     func peripheral(_ peripheral: CBPeripheral, didUpdateNotificationStateFor characteristic: CBCharacteristic, error: Error?) {
@@ -112,10 +110,7 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
             print(error ?? "")
             return
         }
-        //        if characteristic.isNotifying {
-        //            activePeripheral!.readValueForCharacteristic(characteristic)
-        //        }
-        print("update notifcation")
+        print("Update notifcation")
     }
     
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
@@ -130,7 +125,7 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
     
     func centralManager(central: CBCentralManager!,didConnectPeripheral peripheral: CBPeripheral!){
         peripheral.delegate = self
-        peripheral.discoverServices([CBUUID(string: "F000AA00-0451-4000-B000-000000000000")])
+        peripheral.discoverServices([CBUUID(string: "F000AA00-0451-4000-B000-000000000000")])   //all services  --logbox: 0783B03E-8535-B5A0-7140-A304D2495CB7
         output(description: "Connected", data: peripheral.name as AnyObject)
         
     }
@@ -139,6 +134,7 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
         if let servicePeripherals = peripheral.services{
             for servicePeripheral in servicePeripherals{
                 output(description: "Service", data: servicePeripheral.uuid)
+
                 peripheral.discoverCharacteristics(nil, for: servicePeripheral)
                 
             }
@@ -149,41 +145,30 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
    
     func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
         
-//        let characteristicUUID = CBUUID(string: "0783B03E-8535-B5A0-7140-A304D2495CBA")
-//        let properties: CBCharacteristicProperties = [.notify, .read, .Write]
-//        let permissions: CBAttributePermissions = [.readable, .writeable]
-//        let characteristic = CBMutableCharacteristic(
-//            type: characteristicUUID,
-//            properties: properties,
-//            value: nil,
-//            permissions: permissions)
-//        service.characteristics = characteristic
-        
         if let charactericsArr = service.characteristics{
             for charactericsx in charactericsArr{
-                peripheral.setNotifyValue(true, for: charactericsx)
                 
-                print("\n\(charactericsx.properties.rawValue)\n")
+                //notify
+                if charactericsx.uuid.uuidString == "0783B03E-8535-B5A0-7140-A304D2495CB8"{
+                    peripheral.setNotifyValue(true, for: charactericsx)
+                   // peripheral.readValue(for: charactericsx)
+                }
                 
-                //                *************************
+                //read
+                if charactericsx.uuid.uuidString == "0783B03E-8535-B5A0-7140-A304D2495CB9"{
+                  // peripheral.setNotifyValue(true, for: charactericsx)
+                    //peripheral.readValue(for: charactericsx)
+                }
+                
+                //write
                 if charactericsx.uuid.uuidString == "0783B03E-8535-B5A0-7140-A304D2495CBA"{
-                    output(description: "Characteristic", data: charactericsx)
-                    
-                    
-                    //var parameter = NSInteger(1)
-                    var bytes = NSData(bytes: [0x5A, 0xB1, 0xFA, 0xBB, 0xC0, 0x3D, 0xFE, 0x34,0x45,0xCD,0x00,0x54,0x25,0x62,0x36,0x22] as [UInt8], length: 16)
-                    var bytesData = [UInt8]("0x01".utf8)
-                    let writeData = NSData (bytes: &bytes, length: bytesData.count)
+                   self.connectingCharac = charactericsx
+                   
                     let value: [UInt8] = [0x5A, 0xB1, 0xFA, 0xBB, 0xC0, 0x3D, 0xFE, 0x34,0x45,0xCD,0x00,0x54,0x25,0x62,0x36,0x22]
                     let data = NSData(bytes: value, length: 16)
-                    //  let data = "01:00".data(using: String.Encoding.utf8)! as NSData
-                   // let data = NSData(bytes: &bytes, length: 1)
-                    peripheral.writeValue(data as Data, for: charactericsx, type: CBCharacteristicWriteType.withResponse)
-                    output(description: "Characteristic", data: charactericsx)
+                    peripheral.writeValue(data as Data, for: charactericsx, type: CBCharacteristicWriteType.withoutResponse)
                 }
-                //                *************************
                 
-                peripheral.readValue(for: charactericsx)
             }
             
         }
@@ -191,45 +176,34 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
     func peripheral(_ peripheral: CBPeripheral, didWriteValueFor characteristic: CBCharacteristic, error: Error?) {
         
         if error != nil {
-            print("\n***RESPOSTA \n\nDEU RUIM NO WRITE VALUE FOR CHARACTERISTIC    = \(String(describing: error))\nE AQUI TA A PROPRIEDADE DA CHARACTERISTIC \(characteristic.properties.rawValue) \n AQUI TA CHARACTERISTIC INTEIRA \(characteristic)\n\n")
+            print("\n***RESPOSTA \n\nDEU RUIM NO WRITE VALUE FOR CHARACTERISTIC    = \(String(describing: error))\nE AQUI TA A PROPRIEDADE DA CHARACTERISTIC \(characteristic.properties.rawValue) \nAQUI TA CHARACTERISTIC INTEIRA \(characteristic)\n\n")
             return
         }
         
-        print("ENVIEI O PASSWORD\(characteristic)")
-        peripheral.setNotifyValue(true, for: characteristic)
-        print("INFORMACOES DO PERIPHERAL \(peripheral)")
-    }
-    
-    func peripheral(_ peripheral: CBPeripheral, didWriteValueFor descriptor: CBDescriptor, error: Error?) {
-        if error != nil {
-            print("DEU RUIM NO WRITE VALUE FOR DESCRIPTOR    = \(String(describing: error)) ")
-            return
-        }
+        print("\nPASSWORD ENVIADO COM SUCESSO! \nPeripheral = \(peripheral) \nCharacteristic = \(characteristic) \n")
         
-        print("ENVIEI O PASSWORD E ESSE É O DESCRIPTOR \(descriptor)")
     }
     
-
+    
     func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
         if error != nil {
             print("ERROR ON UPDATING VALUE FOR CHARACTERISTIC: \(characteristic) - \(String(describing: error?.localizedDescription))")
             return
         }
     
-        print("ENTREI NO DID UPDATE VALUE FOR CHARACTERISTIC = \(String(describing: peripheral)) E NOTIFY = \(characteristic)")
-        self.connectingCharac = characteristic
-        self.connectingPeripheral = peripheral
+        print("ENTREI NO DID UPDATE VALUE FOR: \nPeripheral = \(String(describing: peripheral)) \nCharacteristic = \(characteristic)")
         
+        //converting "bytes" in string
         let string =  String(data: characteristic.value!, encoding: .utf8)
-        print("VAMOOOOOOOOOOOO\(String(describing: string))")
+        print("Resposta = \(String(describing: string))\n")
         
-//        if let string = String(data: characteristic.value!, encoding: .utf8) {
-//            print(string)
-//        } else {
-//            print("not a valid UTF-8 sequence")
-//        }
+        if string != nil  && string!.range(of:"AOK") == nil{
+            self.response.append(string!)
+        }
+        
     }
     
+   
     func output(description: String, data: AnyObject){
         print("\(description): \(data)")
         textView.text = textView.text + "\(description): \(data)\n"
@@ -242,10 +216,8 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
 extension ViewController: UITableViewDelegate{
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let selectPeripheral = self.peripherals[indexPath.row]
-        //    self.peri = selectPeripheral
         connectBluetooth(peripheral: selectPeripheral)
  
-      //  self.performSegue(withIdentifier: "goConnect", sender: selectPeripheral)
     }
 }
 
